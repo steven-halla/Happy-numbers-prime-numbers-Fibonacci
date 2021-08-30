f70b68c22e99f7601263ea13044e47a8668dec7b
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-30 09:08:14 -0700

    "started first problem"

---
+++ app.js
@@ -0,0 +1,18 @@
+//happy number
+//step 1: do check to see if number is a positive or negative number,set all neg numbers to sad by default using edge cases.
+//step 2: write function to do math, with an exit strategy for when the number reaches 0 or a negative.
+//step 3: make this into a  while loop, with the break condition being when the number reaches 1, 0, or negative.
+//step 4: make variable to store value
+//step 5: pass in number as a paramater
+// step 6: set variable to a boolean value this should help to exit our while loop
+
+function happyNumber(number) {
+  let happyNumber;
+  let happy = false;
+
+  if ( happyNumber < 1) {
+
+  }
+}
+
+happyNumber(19);
