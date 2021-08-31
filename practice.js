75113c14b6f73cbdce6028218eea21bb16769ba5
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-30 20:09:35 -0700

    "finished 1st problem 3rd worksheet"

---
+++ practice.js
@@ -0,0 +1,28 @@
+// may not use the same input twice so no doubling numbers
+
+// for this problem we need to add two elements of an index
+
+// retrieve two elements from an array and then add
+
+// make two params
+
+// use for loop to go through each iteragtion. Since we are comparing two elements in an array we need  a double for loop
+
+// array i and array j should equeal the target
+
+//1
+
+// function returnIndex(target, array) {
+//   for ( let i = 0; i < array.length; i++){
+//     for (let j = 0; j < array.length; j++){
+//       if(array[i] + array[j] == target) {
+//         console.log( [i, j]);
+//         return;
+//       }
+//     }
+//   }
+// }
+//
+// returnIndex(55, [5,17,77,50]);
+
+
